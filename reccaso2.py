fd1ae9955cc9a392164390e3d6d3e294ba76cc71
bojorquezja <bojorquez.ja@gmail.com>
2026-05-21 21:05:01 -0500

    eje 2

--- reccaso2.py
+++ reccaso2.py
@@ -1,3 +1,14 @@
 """
-Hacer una función recursiva en Python que cuente las vocales minúsculas de un texto. 
+Hacer una función recursiva en Python que 
+cuente las vocales minúsculas de un texto. 
 """
+
+def contvoc(tex, x=0):
+    if x == len(tex):
+        return 0
+    else:
+        return (1 if tex[x] in "AeioU" else 0) + contvoc(tex, x+1) 
+
+
+res = input("Ingrese palabra a comntar vocales minusculas")
+print(contvoc(res)) #1
